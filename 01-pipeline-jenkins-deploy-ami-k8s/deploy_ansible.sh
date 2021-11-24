59bc3ffb0bec471a12d72282be754d0f25ece06f
Marianaajpereira <mariana.ajpereira@hotmail.com>
2021-11-24 13:44:16 +0000

    corrigindo path ansible k8s

--- 01-pipeline-jenkins-deploy-ami-k8s/deploy_ansible.sh
+++ 01-pipeline-jenkins-deploy-ami-k8s/deploy_ansible.sh
@@ -1,10 +1,10 @@
-echo "[ec2-dev-img-jenkins]" > ../01-ansible/hosts # cria arquivo
-echo "$(terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../01-ansible/hosts # captura output faz split de espaco e replace de ",
+echo "[ec2-dev-img-jenkins]" > 01-ansible/hosts # cria arquivo
+echo "$(terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> 01-ansible/hosts # captura output faz split de espaco e replace de ",
 
 echo "Aguardando criação de maquinas ..."
 sleep 10 # 10 segundos
 
-cd ../01-ansible
+cd 01-ansible
 
 echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/kp-gamafive.pem ]"
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/kp-gamafive.pem
