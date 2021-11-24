e69e603d122f566bc85de15c69858309fc069ca7
Marianaajpereira <mariana.ajpereira@hotmail.com>
2021-11-24 21:02:16 +0000

    corrigindo bind 12

---
+++ 02-pipeline-jenkins-deploy-ami-db-mysql/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd 02-pipeline-jenkins-deploy-ami-db-mysql/00-terraform
+uri=$(terraform output | grep public_ip | awk '{print $2;exit}' | sed -e "s/\",//g")
+
+echo $uri
+
+ssh -i /var/lib/jenkins/.ssh/kp-gamafive.pem ubuntu@$uri -oStrictHostKeyChecking=no << EOF
+echo \$(pwd)
+version=\$(kubectl version)
+echo "\$version"
+if [[ ! -z "\$version" ]];
+then 
+    echo "::::: MySQL instalado :::::"
+    exit 0
+else
+    echo "::::: MySQL não instalado :::::"
+    exit 1
+fi
+EOF
