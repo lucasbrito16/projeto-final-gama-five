e331dcd736369096f3fb77fc233ec1a079998b3b
Marianaajpereira <mariana.ajpereira@hotmail.com>
2021-11-24 20:31:34 +0000

    corrigindo bind 7

--- 02-pipeline-jenkins-deploy-ami-db-mysql/00-terraform/main.tf
+++ 02-pipeline-jenkins-deploy-ami-db-mysql/00-terraform/main.tf
@@ -36,6 +36,17 @@ resource "aws_security_group" "acesso_db_jenkins_img" {
       self : null
     },
     {
+      description      = "MYSQL from VPC"
+      from_port        = 3306
+      to_port          = 3306
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = null,
+      security_groups : null,
+      self : null
+    },
+    {
       description      = "SSH from VPC"
       from_port        = 80
       to_port          = 80
