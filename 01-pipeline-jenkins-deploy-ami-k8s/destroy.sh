0fca4f9e539937ad1d18a5acd48c01857c55e466
Marianaajpereira <mariana.ajpereira@hotmail.com>
2021-11-24 15:00:17 +0000

    corrigindo test.sh 3

---
+++ 01-pipeline-jenkins-deploy-ami-k8s/destroy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd 01-pipeline-jenkins-deploy-ami-k8s/00-terraform
+terraform destroy -auto-approve
--- 01-pipeline-jenkins-deploy-ami-k8s/test.sh
+++ 01-pipeline-jenkins-deploy-ami-k8s/test.sh
@@ -5,14 +5,10 @@ uri=$(terraform output | grep public_ip | awk '{print $2;exit}' | sed -e "s/\",/
 echo $uri
 
 ssh -i /var/lib/jenkins/.ssh/kp-gamafive.pem ubuntu@$uri -oStrictHostKeyChecking=no << EOF
-
-pwd
-
-version=$(kubectl version)
-
-echo $version
-
-if [[ ! -z $version ]]
+echo \$(pwd)
+version=\$(kubectl version)
+echo "\$version"
+if [[ ! -z "\$version" ]];
 then 
     echo "::::: Kubernets instalado :::::"
     exit 0
@@ -20,5 +16,4 @@ else
     echo "::::: Kubernets não instalado :::::"
     exit 1
 fi
-
 EOF
